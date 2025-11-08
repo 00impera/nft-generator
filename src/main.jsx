7bc3099bad5151cc702ec82123674c1596a41cf0
00impera <nelutz2you@gmail.com>
2025-11-09 00:19:04 +0100

    Add Tailwind CSS configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FarcasterNFT from './FarcasterNFT.jsx'
 import './index.css'
-
-// Simple routing based on URL path
-const getComponent = () => {
-  const path = window.location.pathname
-  
-  // Check if we're in Farcaster context
-  if (path.includes('/farcaster') || window.location.search.includes('farcaster')) {
-    return <FarcasterNFT />
-  }
-  
-  return <App />
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {getComponent()}
+    <App />
   </React.StrictMode>,
 )
