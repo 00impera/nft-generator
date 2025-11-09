e2e6caf5832e872e7c9d824904e7dbc1d09144e6
00impera <nelutz2you@gmail.com>
2025-11-09 16:19:04 +0100

    Updates

---
+++ assets/concat-hex-DP0cNwxG.js
@@ -0,0 +1,1 @@
+import{o as t}from"./index-Bmhli5hI.js";function n(o){return t(...o)}export{n as concatHex};
---
+++ assets/eth_getTransactionCount-rJguhaae.js
@@ -0,0 +1,1 @@
+import{l as n,I as r}from"./index-Bmhli5hI.js";async function u(t,o){const e=await t({method:"eth_getTransactionCount",params:[o.address,o.blockNumber?n(o.blockNumber):o.blockTag||"pending"]});return r(e)}export{u as eth_getTransactionCount};
